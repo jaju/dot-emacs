f41331b88c535abebd1e4bceaa9c48ba5207b137
Ravindra R. Jaju <jaju@msync.org>
2018-03-10 16:51:03 +0530

    Fix bug - assuming current location badly

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ if [ "x$DIRNAME" != "xpersonal" ]; then
     echo "Looks like I am in a bad location. Should be inside of a directory named 'personal' - and meant to be inside of the prelude environs"
     exit 1
 fi
-cd ..
+cd $THISDIR/..
 for f in custom prelude-modules; do
     ln -s personal/$f.le $f.el
 done
